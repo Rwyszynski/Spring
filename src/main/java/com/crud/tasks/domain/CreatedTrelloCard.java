23eea27ce90804a4d3a88c61436bfdbc235aeb16
Robert Wyszynski <robertointerwento@gmail.com>
2025-01-24 13:44:11 +0100

    21.3. Client development

---
+++ src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
@@ -0,0 +1,43 @@
+package com.crud.tasks.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CreatedTrelloCard {
+
+    @JsonProperty("id")
+    private String id;
+
+    @JsonProperty("badges")
+    private Badges badges;
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Badges {
+        @JsonProperty("votes")
+        private int votes;
+
+        @JsonProperty("attachmentsByType")
+        private AttachmentsByType attachmentsByType;
+    }
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class AttachmentsByType {
+        @JsonProperty("trello")
+        private Trello trello;
+    }
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Trello {
+        @JsonProperty("board")
+        private int board;
+
+        @JsonProperty("card")
+        private int card;
+    }
+}
