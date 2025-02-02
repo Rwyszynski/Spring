7693bd2c05fb965004c8dafc088299e469ebf5ee
Robert Wyszynski <robertointerwento@gmail.com>
2025-02-02 08:43:11 +0100

    21.4. Preparing endpoint for export tasks

--- src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
+++ src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
@@ -2,8 +2,29 @@ package com.crud.tasks.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CreatedTrelloCard {
+
+    @JsonProperty("id")
+    private String id;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("shortUrl")
+    private String shortUrl;
+
+
+}
+/*
+@Getter
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CreatedTrelloCard {
@@ -41,3 +62,5 @@ public class CreatedTrelloCard {
         private int card;
     }
 }
+
+ */
